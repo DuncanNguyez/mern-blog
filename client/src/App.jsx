7c0cf00e46ff087246598616cd4fe3336890f784
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-07-13 22:31:17 +0700

    client: setup client and init pages, routes and header

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
