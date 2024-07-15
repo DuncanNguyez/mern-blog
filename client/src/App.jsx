fe0dde53632f8a6fb65ff96fc6d22c135f01d072
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-07-15 15:14:35 +0700

    client: create Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import FooterCom from "./components/Footer";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
+      <FooterCom></FooterCom>
     </BrowserRouter>
   );
 }
