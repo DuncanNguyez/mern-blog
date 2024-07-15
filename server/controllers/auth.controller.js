e8f72d55d3d0d2c74ec00da14a5848c01e416d33
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-07-15 17:58:21 +0700

    server: create sign in api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
@@ -19,6 +20,31 @@ export const signup = async (req, res, next) => {
     const hashPassword = bcryptjs.hashSync(password, 10);
     await User.create({ username, email, password: hashPassword });
     return res.status(201).json({ message: "Signup successful!" });
+  } catch (error) {
+    return next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password || username === "" || password === "") {
+    return next(errorHandler(400, "All fields are required!"));
+  }
+
+  try {
+    const user = await User.findOne({ username }).lean();
+    if (!user) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const validPw = bcryptjs.compareSync(password, user.password);
+    if (!validPw) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json({ message: "Sign in successful" });
   } catch (error) {
     return next(error);
   }
