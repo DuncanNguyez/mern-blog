704f81a574fcb6438c84603f96a10156db2561b4
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-07-16 17:30:18 +0700

    client: add theme to ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </PersistGate>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice.js";
 import persistReducer from "redux-persist/es/persistReducer";
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
 import createTransform from "redux-persist/es/createTransform";
 
+import userReducer from "./user/userSlice.js";
+import themeReducer from "./theme/themSlice.js";
+
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const transform = createTransform(
