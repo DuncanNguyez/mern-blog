9180b6b5063760d2f6b92868af586d94ff760b6b
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-07-17 02:35:22 +0700

    client: create profile page

--- client/src/pages/dashboard/Profile.jsx
+++ client/src/pages/dashboard/Profile.jsx
@@ -1,6 +1,40 @@
+import { Button, TextInput } from "flowbite-react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="font-semibold my-7 text-center">Profile</h1>
+      <form className="flex flex-col gap-3 my-3" action="">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser.imageUrl}
+            alt="user"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput type="password" id="password" placeholder="password" />
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 flex justify-between">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
