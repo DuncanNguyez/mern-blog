981149cd9d1d5cd5ca444e49b624949a4b02fcab
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-08-07 03:35:07 +0700

    Client: add delete image handle

--- client/src/pages/dashboard/CreatePost/PostEditor.jsx
+++ client/src/pages/dashboard/CreatePost/PostEditor.jsx
@@ -2,6 +2,7 @@ import { RichTextEditor } from "mui-tiptap";
 import { useDispatch, useSelector } from "react-redux";
 import { getAuth } from "firebase/auth";
 import {
+  deleteObject,
   getDownloadURL,
   getStorage,
   ref,
@@ -16,9 +17,48 @@ import { app } from "../../../firebase";
 export default function PostEditor() {
   const { editorDoc } = useSelector((state) => state.draft);
   const dispatch = useDispatch();
-  const handleChangeEditor = ({ editor }) => {
-    dispatch(updateDraftEditor(editor.getJSON()));
+  const findImageNode = (node, imagesData) => {
+    if (!node) {
+      return;
+    }
+    if (node?.type?.name === "image") {
+      const { src } = node.attrs;
+      imagesData.add(src);
+      return;
+    }
+    const arr = Array.isArray(node.content)
+      ? node.content
+      : node.content.content;
+    arr.map((item) => findImageNode(item, imagesData));
   };
+  const deleteImage = async (transaction) => {
+    let setSrc = new Set();
+    let beforeSetSrc = new Set();
+    transaction.doc.forEach((node) => {
+      findImageNode(node, setSrc);
+    });
+    transaction.before.forEach((node) => {
+      findImageNode(node, beforeSetSrc);
+    });
+
+    beforeSetSrc.forEach((src) => {
+      if (!setSrc.has(src)) {
+        getAuth(app);
+        const storage = getStorage(app);
+        try {
+          const desertRef = ref(storage, src);
+          deleteObject(desertRef);
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+    });
+  };
+  const handleChangeEditor = ({ editor, transaction }) => {
+    dispatch(updateDraftEditor(editor.getJSON()));
+    deleteImage(transaction);
+  };
+
   const uploadImage = async (files) => {
     getAuth(app);
     const storage = getStorage(app);
@@ -44,7 +84,6 @@ export default function PostEditor() {
                 }
               );
             });
-            console.log(url);
             return { src: url, alt: "" };
           } catch (error) {
             alert("image has not been uploaded yet (note: image size < 2M)");
