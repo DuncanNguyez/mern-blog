b877b147f9810a21cd02991f254f6b370309bab1
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-09-02 18:33:26 +0700

    server: fix: process init db

--- dbinit/init.sh
+++ dbinit/init.sh
@@ -3,18 +3,30 @@ echo "start"
 
 export $(grep -v '^#' ../server/.env)
 
-MONGO_VOLUME="server_blog-mongodb"
-ELS_VOLUME="server_blog-els"
+loglineMongosh=$(
+    mongosh --port 27018 --username $MONGO_USERNAME --password $MONGO_PASSWORD --authenticationDatabase admin <<EOF
+use('blog-app');
+db.posts.find().count();
+EOF
+)
+mongoPostCount=$(echo "$loglineMongosh" | tail -n 2 | grep -Eo '[0-9]+')
 
-if docker volume ls | grep -q $MONGO_VOLUME; then
-    echo "$MONGO_VOLUME volume exists"
+echo "mongoPost: $mongoPostCount"
+
+if [ "$mongoPostCount" -gt 0 ]; then
+    echo "has been db"
 else
     echo "init mongodb"
     mongorestore --host localhost --port 27018 --username $MONGO_USERNAME --password $MONGO_PASSWORD --db blog-app --authenticationDatabase admin --archive=mongo.init --gzip
 fi
 
-if docker volume ls | grep -q $ELS_VOLUME; then
-    echo "$ELS_VOLUME volume exists"
+loglineEls=$(curl -s -X GET "http://localhost:9201/post/_count" -H 'Content-Type: application/json')
+elsPostCount=$(echo "$loglineEls" | grep -Eo '"count":[0-9]+' | grep -Eo '[0-9]+')
+
+echo "elsPost: $elsPostCount"
+
+if [ "$elsPostCount" -gt 0 ]; then
+    echo "has been els db"
 else
     echo "init elsdb"
     elasticdump --input elsinit.json --output http://localhost:9201
