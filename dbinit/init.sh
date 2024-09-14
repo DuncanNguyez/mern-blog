a110e0a4f76298361ea0bf7979ec533a03e95c55
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-09-14 16:02:51 +0700

    test: jenkinfile

--- dbinit/init.sh
+++ dbinit/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "start"
-
+set -x 
 # host.docker.internal
 host=$1
 if [ -z "$1" ]; then
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
 fi
 
 export $(grep -vE '^\s*#|^\s*$' ../server/.env | xargs)
-
+echo "$MONGO_USERNAME"
 mongoPostCount=$(
     docker exec blog-mongo mongosh --port 27017 --username $MONGO_USERNAME --password $MONGO_PASSWORD --authenticationDatabase admin --eval "use('blog-app');db.posts.find().count();"
 )
