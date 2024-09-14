da9214c80233856d8642e75131ac657b596c5d44
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-09-14 17:27:35 +0700

    test jenkinfile:

--- dbinit/init.sh
+++ dbinit/init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "start"
-set -x 
 # host.docker.internal
 host=$1
 if [ -z "$1" ]; then
-    host= "localhost"
+    host="localhost"
 fi
 
 export $(grep -vE '^\s*#|^\s*$' ../server/.env | xargs)
@@ -15,12 +14,12 @@ mongoPostCount=$(
 
 echo "mongoPost: $mongoPostCount"
 
-if [ "$mongoPostCount" -gt 0 ]; then
-    echo "has been db"
-else
+if [ "$mongoPostCount" -eq 0 ]; then
     echo "init mongodb"
     docker cp mongo.init blog-mongo:mongo.init
     docker exec blog-mongo mongorestore --host localhost --port 27017 --username $MONGO_USERNAME --password $MONGO_PASSWORD --db blog-app --authenticationDatabase admin --archive=mongo.init --gzip
+else
+    echo "has been db"
 fi
 
 loglineEls=$(curl -s -X GET "http://$host:9201/post/_count" -H 'Content-Type: application/json')
@@ -28,11 +27,11 @@ elsPostCount=$(echo "$loglineEls" | grep -Eo '"count":[0-9]+' | grep -Eo '[0-9]+
 
 echo "elsPost: $elsPostCount"
 
-if [ "$elsPostCount" -gt 0 ]; then
-    echo "has been els db"
-else
+if [ "$elsPostCount" -eq 0 ]; then
     echo "init elsdb"
     npx elasticdump --input elsinit.json --output http://$host:9201
+else
+    echo "has been els db"
 fi
 
 echo "done"
